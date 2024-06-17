697017f91c776bdcb70f6080b46f07e1542b83e1
yangfan_hailv <fan.yang@elestyle.jp>
2024-06-17 19:02:09 +0900

    feat: empty proj

--- Pods/Target Support Files/Pods-ELEPayExample-ELEPayExampleUITests/Pods-ELEPayExample-ELEPayExampleUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ELEPayExample_ELEPayExampleUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ELEPayExample_ELEPayExampleUITests
-@end
--- Pods/Target Support Files/Pods-ELEPayExampleTests/Pods-ELEPayExampleTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ELEPayExampleTests : NSObject
-@end
-@implementation PodsDummy_Pods_ELEPayExampleTests
-@end
